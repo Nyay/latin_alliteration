4c56fe2f379f89b7e5aa00f5f4c3c35b70aabc1c
Asya Simonyan <anniesim97@gmail.com>
2016-12-26 14:06:48 +0300

    second commit Исправно вычисляется коэффициент аллитерации-3 И создаются csv-файлы

--- alliter_programm.py
+++ alliter_programm.py
@@ -12,49 +12,63 @@ import os
 
 
 def open_files():
-    for a, b, c in os.dirs():
-        pairs = read_file(a)
-        write_csv(pairs, a)
-    pass
+	for root, dirs, files in os.walk('./texts'):
+		for fl in files:
+			f_name = './texts/' + fl
+			pairs = read_file(f_name)
+			write_csv(pairs, f_name)
 
 
 def read_file(f_name):
-    fr = open(f_name, 'r', encoding='UTF-8')
-    arr = f.readlines()
-    pairs = []
+	fr = open(f_name, 'r', encoding='UTF-8')
+	arr = fr.readlines()
+	pairs = make_pairs(arr)
+	fr.close()
+	return pairs
+
+
+def write_csv(pairs, name):
+	name = name.lower()
+	n_name = name.replace('txt', 'csv')
+	n_name = n_name.replace('texts', 'tables')
+	f = open(n_name, 'w', encoding='UTF-8')
+	for pair in pairs:
+		f.write(str(pair[0]) + ',' + pair[1] + '\n')
+	f.close()
+
+
+def make_pairs(arr):
+	pairs = []
 	for line in arr:
 		pair = process_line(line)
 		pairs.append(pair)
-	fr.close()
-	fw = open(f_name, 'a', encoding='UTF-8')
-	
-	fw.close()
 	return pairs
 
 
 def process_line(l):
+	line1 = change_uv(l)
 	letter_set = 'bcdfgklmnpqrstvwxz'
 	d = {}
 	for letter in letter_set:
-		num = l.count(letter)
+		num = line1.count(letter)
 		d[num] = letter
-	return d[max(d)], max(d)
+	return max(d), d[max(d)]
 
 
-def count_letter(sym, line):
-	k = 0
-	for letter in line:
-		if letter == sym:
-			k += 1
-	return k
+def change_uv(line):
+	line = line.lower()
+	line = line.replace('ngv', 'ngu')
+	line = line.replace('qv', 'qu')
+	line = line.replace('sv', 'su')
+	return line
 
 
-def write_csv(pairs, name):
-	n_name = name.replace('txt', 'csv')
-	f = open(n_name, 'w', encoding='UTF-8')
-	for pair in pairs:
-		f.write(pair[0] + ',' + pair[1])
-	f.close()
+def main():
+	open_files()
+
+
+if __name__ == '__main__':
+	main()
 
 
 #df = pd.read_csv(n_name)
